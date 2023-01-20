6d28a40f6b97b102ce9d32014e39dd6aba336317
Westsilver <seo606101@naver.com>
2023-01-20 12:53:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+
+  }
+  50%{
+    
+    border-radius:100px;
+
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+
+`;
+const Emoji = styled.span`
+  font-size: 50px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 100px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji as="p">😊</Emoji>
+      </Box>
+      <Emoji as="p">🦁</Emoji>
+    </Wrapper>
   );
 }
 
